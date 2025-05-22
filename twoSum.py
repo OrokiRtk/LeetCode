226ebcf1928bcdc2180fd4cab4ce1bcb0b3081bc
RenanOrok <orokigameplay@gmail.com>
2025-05-22 20:33:38 -0300

    Segundo commit

--- twoSum.py
+++ twoSum.py
@@ -1,4 +1,10 @@
 #Dado um array de números inteiros nums e um alvo inteiro, retorne os índices dos dois números de forma que a soma deles resulte no alvo.
+
+#Example 1:
+#Input: nums = [2,7,11,15], target = 9
+#Output: [0,1]
+#Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 def twoSum(self, nums, target):
         numMap = {}
         for i, num in enumerate(nums):
